068bc4c36625e33eab4cc313f32be52055fe0c67
SumiAkter2 <akteralisha36@gmail.com>
2022-08-25 19:06:09 +0600

    added task

--- src/Component/DailyTask.js
+++ src/Component/DailyTask.js
@@ -2,6 +2,14 @@ import React from "react";
 import logo from "../todo.png";
 import "../App.css";
 const DailyTask = () => {
+  const inputValue = (e) => {
+    console.log(e.target.value);
+  };
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    const add = e.target.name.value;
+    console.log("add", add);
+  };
   return (
     <div>
       <div className="flex p-4 bg-slate-200">
@@ -10,11 +18,18 @@ const DailyTask = () => {
       </div>
       <div className="flex justify-center items-center my-32">
         <input
+          onBlur={inputValue}
+          name="text"
           type="text"
           placeholder="Type here"
-          class="input input-bordered input-info w-full max-w-xs "
+          className="input input-bordered input-info w-full max-w-xs "
         />
-        <button class="btn  btn-outline btn-info p-4 ml-4">Add</button>
+        <button
+          onSubmit={handleAddTask}
+          className="btn  btn-outline btn-info p-4 ml-4"
+        >
+          Add
+        </button>
       </div>
     </div>
   );
