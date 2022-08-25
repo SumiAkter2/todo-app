81dbb15826cf21ad7965506f0536125b366e379f
SumiAkter2 <akteralisha36@gmail.com>
2022-08-25 18:58:50 +0600

    added input

--- src/Component/DailyTask.js
+++ src/Component/DailyTask.js
@@ -4,9 +4,17 @@ import "../App.css";
 const DailyTask = () => {
   return (
     <div>
-      <div className="flex">
-        <img className="App" src={logo} alt="" />
-        <h1 className="text-slate-600 text-6xl ">todo </h1>
+      <div className="flex p-4 bg-slate-200">
+        <img className="App w-16 shrink-0" src={logo} alt="" />
+        <h1 className="text-slate-600 text-6xl pl-2">todo </h1>
+      </div>
+      <div className="flex justify-center items-center my-32">
+        <input
+          type="text"
+          placeholder="Type here"
+          class="input input-bordered input-info w-full max-w-xs "
+        />
+        <button class="btn  btn-outline btn-info p-4 ml-4">Add</button>
       </div>
     </div>
   );
